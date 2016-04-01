77491476ff730e829af6cc28f4c2d6d244a93ea9
likaci <likaci@qq.com>
2016-04-01 11:18:36 +0800

    练习 Service

--- app/src/main/java/com/xiazhiri/practice/MainActivity.java
+++ app/src/main/java/com/xiazhiri/practice/MainActivity.java
@@ -1,21 +1,13 @@
 package com.xiazhiri.practice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action0;
-import rx.functions.Action1;
-import rx.functions.Func1;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,46 +24,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                startService(intent);
             }
         });
-
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Observable.just(null)
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .map(new Func1<Object, Object>() {
-                            @Override
-                            public Object call(Object o) {
-                                Log.e(TAG, "map1: " + Thread.currentThread().getName());
-                                return null;
-                            }
-                        })
-                        .observeOn(Schedulers.io())
-                        .map(new Func1<Object, Object>() {
-                            @Override
-                            public Object call(Object o) {
-                                Log.e(TAG, "map2: " + Thread.currentThread().getName());
-                                return null;
-                            }
-                        })
-                        .doOnSubscribe(new Action0() {
-                            @Override
-                            public void call() {
-                                Log.e(TAG, "onSubscribe: " + Thread.currentThread().getName());
-                            }
-                        })
-                        .subscribe(new Action1<Object>() {
-                            @Override
-                            public void call(Object o) {
-                                Log.e(TAG, "subscribe: " + Thread.currentThread().getName());
-                            }
-                        });
-            }
-        }).start();
 
     }
 
@@ -96,4 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
